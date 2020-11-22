a21dfc4338b21eb61ea018211dd5915d39233bb4
timdancan <saqe789@gmail.com>
2020-11-22 13:33:38 +0200

    1.0

---
+++ index.js
@@ -0,0 +1,29 @@
+let editButtonNode = document.querySelector('.edit-button');
+let popupNode = document.querySelector('.popup');
+let closeButtonNode = document.querySelector('.popup__close');
+
+let profileTitleNode = document.querySelector('.profile__title');
+let profileSubtitleNode = document.querySelector('.profile__subtitle');
+let popupFormNode = document.querySelector('.popup__form');
+
+editButtonNode.addEventListener('click', editClick);
+closeButtonNode.addEventListener('click', closeClick);
+
+function editClick() {
+  popupNode.classList.add('popup_visiable');
+}
+
+function closeClick() {
+  popupNode.classList.remove('popup_visiable');
+}
+
+popupFormNode.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let popupInputTitleNode = document.querySelector('.popup__input_place_title');
+  let popupInputSubtitleNode = document.querySelector('.popup__input_place_subtitle');
+  profileTitleNode.textContent = popupInputTitleNode.value;
+  profileSubtitleNode.textContent = popupInputSubtitleNode.value;
+}
+
