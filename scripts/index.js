e8dbafa3896414b57f26aeff4d71e26c95323550
timdancan <saqe789@gmail.com>
2020-11-23 10:40:52 +0200

    1.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,16 @@
-let editButtonNode = document.querySelector('.edit-button');
+let editButtonNode = document.querySelector('.profile__edit-button');
 let popupNode = document.querySelector('.popup');
 let closeButtonNode = document.querySelector('.popup__close');
 
 let profileTitleNode = document.querySelector('.profile__title');
 let profileSubtitleNode = document.querySelector('.profile__subtitle');
 let popupFormNode = document.querySelector('.popup__form');
-
-editButtonNode.addEventListener('click', editClick);
-closeButtonNode.addEventListener('click', closeClick);
+let popupInputTitleNode = document.querySelector('.popup__input_place_title');
+let popupInputSubtitleNode = document.querySelector('.popup__input_place_subtitle');
 
 function editClick() {
+  document.querySelector('.popup__input_place_title').value = profileTitleNode.textContent;
+  document.querySelector('.popup__input_place_subtitle').value = profileSubtitleNode.textContent;
   popupNode.classList.add('popup_visiable');
 }
 
@@ -17,16 +18,13 @@ function closeClick() {
   popupNode.classList.remove('popup_visiable');
 }
 
-popupFormNode.addEventListener('submit', handleFormSubmit);
-
-document.querySelector('.popup__input_place_title').value = 'Жак-Ив Кусто';
-document.querySelector('.popup__input_place_subtitle').value = 'Исследователь океана';
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let popupInputTitleNode = document.querySelector('.popup__input_place_title');
-  let popupInputSubtitleNode = document.querySelector('.popup__input_place_subtitle');
   profileTitleNode.textContent = popupInputTitleNode.value;
   profileSubtitleNode.textContent = popupInputSubtitleNode.value;
+  closeClick();
 }
 
+popupFormNode.addEventListener('submit', handleFormSubmit);
+editButtonNode.addEventListener('click', editClick);
+closeButtonNode.addEventListener('click', closeClick);
