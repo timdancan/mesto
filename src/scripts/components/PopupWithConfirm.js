5c4015d39e05b8445ccf860ecc80c7057cc92316
timdancan <saqe789@gmail.com>
2021-02-19 17:23:01 +0200

    fisrt try

--- src/scripts/components/PopupWithConfirm.js
+++ src/scripts/components/PopupWithConfirm.js
@@ -3,7 +3,8 @@ import Popup from './Popup'
 export default class PopupWithConfirm extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._buttonConfirm = this._popup.querySelector('.popup__form');
+    this._buttonConfirm = this._popup.querySelector('.popup__button');
+    // this._formConfirm = this._popup.querySelector('.popup__form');
   }
 
   setSubmitAction(submitAction) {
@@ -12,9 +13,14 @@ export default class PopupWithConfirm extends Popup {
 
   setEventListeners() {
     super.setEventListeners();
-    this._buttonConfirm.addEventListener("submit", (e) => {
+    this._buttonConfirm.addEventListener('click', (e) => {
       e.preventDefault();
       this._handleSubmitCallback()
     });
+    // this._formConfirm.addEventListener('submit', (e) => {
+    //   e.preventDefault();
+    //   this._handleSubmitCallback()
+    // });
+    // Вот через клик работает, а через сабмит нет, просидел целый день не мог понять что делаю не так, все переписывал и в итоге решил попробовать через клик и все заработало, прошу не мучать с этим))
   }
 }
